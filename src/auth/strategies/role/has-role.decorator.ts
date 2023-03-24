fef9c7a1dcf70803c5e224d892c293cea9da1154
tomervar <73695635+tomervar@users.noreply.github.com>
2023-03-24 10:37:04 +0300

    Delete src directory

--- src/auth/strategies/role/has-role.decorator.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { SetMetadata } from '@nestjs/common';
-import { Role } from '../../../users/role.enum';
-
-export const HasRoles = (...roles: Role[]) => SetMetadata('roles', roles);
